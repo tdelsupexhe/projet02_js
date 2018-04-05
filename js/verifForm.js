0cb42327b58574f4f4f04f5081dac7510db8d0ef
Thomas Delsupexhe <tdelsupexhe@gmail.com>
2018-04-05 19:37:18 +0200

    Update verifForm.js

--- js/verifForm.js
+++ js/verifForm.js
@@ -80,29 +80,42 @@ function verifDate(date, varThis){
         }
 }
 
+/**
+ *
+ * @param varThis
+ */
 function removeErrors(varThis){
     $('#' + varThis['0'].id).removeClass('bg-danger text-white');
     $('#error_' + varThis['0'].id).text('');
 }
 
-function addEleves(nomForm){
-    var form;
-    form = $('#' + nomForm);
+/**
+ *
+ * @param nomForm
+ */
+function addEleves(nomForm) {
+    var form = $('#' + nomForm);
+    var idStudent = form['0']['9'].value;
+    var resultId ;
+    var tableBody = $('#tableBody');
 
-    /*var nbChamps;
-    var i;
-    var txt;
-
-    nbChamps = form['0'].length;
-
-    for(i=0; i<nbChamps; i++){
-        txt += '<td>' + form['0']['i'].value + '</td>';
-    }
-
-    console.log(txt);*/
-
-
-    $('#tableBody').append('<tr>' +
+    if (idStudent === '') {
+        resultId = tableBody['0'].childElementCount;
+        tableBody.append('<tr id="student_' + resultId +'">' +
+            '<td>' + resultId + '</td>' +
+            '<td>' + form['0']['1'].value + '</td>' +
+            '<td>' + form['0']['0'].value + '</td>' +
+            '<td>' + form['0']['6'].value + '</td>' +
+            '<td>' + form['0']['2'].value + '</td>' +
+            '<td>' + form['0']['3'].value + '</td>' +
+            '<td>' + form['0']['4'].value + '</td>' +
+            '<td>' + form['0']['5'].value + '</td>' +
+            '<td>' + form['0']['7'].value + '</td>' +
+            '<td>' + form['0']['8'].value + '</td>' +
+            '</tr>');
+    } else {
+    $('#student_' + idStudent).html(
+        '<td>' + idStudent + '</td>' +
         '<td>' + form['0']['1'].value + '</td>' +
         '<td>' + form['0']['0'].value + '</td>' +
         '<td>' + form['0']['6'].value + '</td>' +
@@ -111,8 +124,9 @@ function addEleves(nomForm){
         '<td>' + form['0']['4'].value + '</td>' +
         '<td>' + form['0']['5'].value + '</td>' +
         '<td>' + form['0']['7'].value + '</td>' +
-        '<td>' + form['0']['8'].value + '</td>' +
-        '</tr>');
+        '<td>' + form['0']['8'].value + '</td>'
+        );
+    }
 }
 
 function clearForm(){
